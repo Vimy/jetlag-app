38e23429a075c6a3a215fb53d3f6a35426601b3c
MatthiasV <matthias.vermeulen@gmail.com>
2014-11-03 01:22:25 +0100

    Initial Commit

---
+++ Jetlag-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jetlag-app
+//
+//  Created by Matthias Vermeulen on 3/11/14.
+//  Copyright (c) 2014 Noizy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
